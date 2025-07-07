7c9fb92ff4c474c653c830cf2d2e5e2b05fd37b5
AlxRpp <alexanderr26@outlook.de>
2025-07-07 21:49:21 +0200

    improved the folder structure for the assets

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSection } from "./hero-section/hero-section";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSection],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
