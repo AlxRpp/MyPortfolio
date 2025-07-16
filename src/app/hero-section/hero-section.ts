46c445ca633227b3a98b5462084d184fb9b41bee
AlxRpp <alexanderr26@outlook.de>
2025-07-16 07:37:01 +0200

    added CTA-Button to Hero-Section

--- src/app/hero-section/hero-section.ts
+++ src/app/hero-section/hero-section.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../shared/header/header";
+import { Button } from "../shared/button/button";
 
 @Component({
   selector: 'app-hero-section',
-  imports: [Header],
+  imports: [Header, Button],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss'
 })
