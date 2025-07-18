e9eef41178987449a780c6d88c8676e7e7459714
AlxRpp <alexanderr26@outlook.de>
2025-07-18 20:30:19 +0200

    added about-me Component

---
+++ src/app/about-me/about-me.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Button } from "../shared/button/button";
+
+@Component({
+  selector: 'app-about-me',
+  imports: [Button],
+  templateUrl: './about-me.html',
+  styleUrl: './about-me.scss'
+})
+export class AboutMe {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSection } from "./hero-section/hero-section";
+import { AboutMe } from "./about-me/about-me";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroSection],
+  imports: [RouterOutlet, HeroSection, AboutMe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
