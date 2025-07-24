2630f88780cd7920c1ed81b50c72a01cdb87c621
AlxRpp <alexanderr26@outlook.de>
2025-07-24 21:36:52 +0200

    added skills component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSection } from "./hero-section/hero-section";
 import { AboutMe } from "./about-me/about-me";
+import { Skills } from "./skills/skills";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroSection, AboutMe],
+  imports: [RouterOutlet, HeroSection, AboutMe, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
