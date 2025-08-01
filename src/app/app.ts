70636532519a41d518649d24de87c99c2556e25e
AlxRpp <alexanderr26@outlook.de>
2025-08-01 09:07:07 +0200

    added Projects-Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeroSection } from "./hero-section/hero-section";
 import { AboutMe } from "./about-me/about-me";
 import { Skills } from "./skills/skills";
+import { Projects } from "./projects/projects";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroSection, AboutMe, Skills],
+  imports: [RouterOutlet, HeroSection, AboutMe, Skills, Projects],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
