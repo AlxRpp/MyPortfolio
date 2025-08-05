9e0ad50dcd23cae86de3fb39baa843420c0baed8
AlxRpp <alexanderr26@outlook.de>
2025-08-05 21:36:13 +0200

    added toggle to custom Checkbox

--- src/app/collaborate/collaborate.ts
+++ src/app/collaborate/collaborate.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Button } from "../shared/button/button";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-collaborate',
-  imports: [Button],
+  imports: [Button, CommonModule],
   templateUrl: './collaborate.html',
   styleUrl: './collaborate.scss'
 })
 export class Collaborate {
+
+  checked = signal<boolean>(false)
+
+  toggleCheckbox() {
+    this.checked.update(value => !value);
+  }
 
   test() {
     console.log('Hallo');
