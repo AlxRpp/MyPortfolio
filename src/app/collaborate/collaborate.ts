0848738dcc12e614f4896dd7b18dbb58980ff610
AlxRpp <alexanderr26@outlook.de>
2025-08-06 09:05:38 +0200

    small improvements at the hover Inputs

--- src/app/collaborate/collaborate.ts
+++ src/app/collaborate/collaborate.ts
@@ -1,24 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { Button } from "../shared/button/button";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-collaborate',
-  imports: [Button, CommonModule],
+  imports: [Button, CommonModule, FormsModule],
   templateUrl: './collaborate.html',
   styleUrl: './collaborate.scss'
 })
 export class Collaborate {
 
   checked = signal<boolean>(false)
+  name = signal<string>("");
 
   toggleCheckbox() {
     this.checked.update(value => !value);
   }
 
   test() {
-    console.log('Hallo');
-
+    console.log(this.name());
   }
 
 }
