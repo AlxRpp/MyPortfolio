358a894d5eee348646f41fa6514d452eab853098
AlxRpp <alexanderr26@outlook.de>
2025-08-07 21:15:43 +0200

    added animation at footer

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './footer.scss'
 })
 export class Footer {
+  git = viewChild.required<HTMLImageElement>('git');
+  linkedIn = viewChild.required<HTMLImageElement>('linkedIn');
+  mail = viewChild.required<HTMLImageElement>('mail');
 
+
+  // rollingIn() {
+  //   this.git().nativeElement.classList.remove('roll-out')
+  //   this.git().nativeElement.classList.add('roll-in')
+  // }
+
+  // rollingOut() {
+  //   this.git().nativeElement.classList.remove('roll-in')
+  //   this.git().nativeElement.classList.add('roll-out')
+
+  // }
+
+  toggleAnimation(ref: HTMLImageElement, direction: 'in' | 'out') {
+    // const id = ref.nativeElement;
+    ref.classList.add(direction === 'in' ? 'roll-in' : 'roll-out');
+    ref.classList.remove(direction === 'out' ? 'roll-in' : 'roll-out');
+
+  }
 }
