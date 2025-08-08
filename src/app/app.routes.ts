a5b37efe12cd7321d95eb2d39ee3eb548f99644a
AlxRpp <alexanderr26@outlook.de>
2025-08-08 19:33:41 +0200

    added policy´s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './main/pages/home/home';
 import { SingleProject } from './main/pages/single-project/single-project';
+import { LegalNotice } from './main/pages/legal-notice/legal-notice';
+import { PrivacyPolicy } from './main/pages/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
     { path: "", component: Home },
     { path: "projects/:slug", component: SingleProject },
+    { path: "legal-notice", component: LegalNotice },
+    { path: "privacy-policy", component: PrivacyPolicy },
     { path: "**", redirectTo: "" }
 ];
