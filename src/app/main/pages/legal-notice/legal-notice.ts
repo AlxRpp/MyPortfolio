170cffbe7b01e075605754d1ca84fd6776de61b2
AlxRpp <alexanderr26@outlook.de>
2025-08-13 16:30:30 +0200

    added legal notice

--- src/app/main/pages/legal-notice/legal-notice.ts
+++ src/app/main/pages/legal-notice/legal-notice.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { Footer } from '../../shared/footer/footer';
+import { TranslateDirective, TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [Header, Footer],
+  imports: [Header, Footer, TranslatePipe, TranslateDirective, CommonModule],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss'
 })
 export class LegalNotice {
+  private translate = inject(TranslateService)
 
 }
