0016b99ef9e1473ef728fb302d7632e65159f540
AlxRpp <alexanderr26@outlook.de>
2025-08-13 16:09:54 +0200

    added privacy policy

--- src/app/main/pages/privacy-policy/privacy-policy.ts
+++ src/app/main/pages/privacy-policy/privacy-policy.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { Footer } from '../../shared/footer/footer';
+import { TranslateDirective, TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [Header, Footer],
+  imports: [CommonModule, Header, Footer, TranslatePipe, TranslateDirective],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss'
 })
 export class PrivacyPolicy {
-
+  private translate = inject(TranslateService)
 }
