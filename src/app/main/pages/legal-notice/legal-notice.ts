7386f6ab696f9affec4209cbc00e6824a6f8e6e6
AlxRpp <alexanderr26@outlook.de>
2025-08-13 14:44:29 +0200

    added design layout to policy´s pages

--- src/app/main/pages/legal-notice/legal-notice.ts
+++ src/app/main/pages/legal-notice/legal-notice.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
+
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss'
 })
--- src/app/main/pages/privacy-policy/privacy-policy.ts
+++ src/app/main/pages/privacy-policy/privacy-policy.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss'
 })
